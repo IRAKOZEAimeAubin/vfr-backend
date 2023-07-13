7e1055b4639c04fa032cbbac5a09f0fe7431cbe3
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-07-13 10:15:11 +0200

    fix: jwt key fix

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,11 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext): boolean {
-    const { user } = context.switchToHttp().getRequest();
-
-    return user.role !== 'USER';
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
