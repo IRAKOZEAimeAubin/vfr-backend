52096f1edb789d15f0e13241559bf531a5063d62
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-09-02 18:08:53 +0200

    feat: change expires in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ export const jwtSecret = process.env.JWT_SECRET;
     PassportModule,
     JwtModule.register({
       secret: jwtSecret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '100y' },
     }),
     UsersModule,
   ],
